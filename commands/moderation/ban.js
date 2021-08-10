c46f2f1f212e26220fe170aba1251d0fba595b42
Fall <86908573+riflefal@users.noreply.github.com>
2021-08-10 09:58:45 -0300

    Update ban.js
    Actualización de diálogos.

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -11,19 +11,19 @@ module.exports = {
     
     const reason = args.slice(1).join(" ")
     
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`You don't have enough powers to ban someone`)
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`No tienes permisos para banear a alguien`)
     
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply(`I don't have powers to ban someone`)
+    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply(`No tengo permisos para banear a alguien`)
     
-    if(!args[0]) return message.reply(`Please mention someone to ban`)
+    if(!args[0]) return message.reply(`Da una razòn para el baneo`)
     
-    if(!target) return message.reply(`I can't find that member`)
+    if(!target) return message.reply(`No puedo encontrar a ese miembro`)
     
     if(target.roles.highest.position >= message.member.roles.highest.position || message.author.id !== message.guild.owner.id) {
       return message.reply(`They have more power than you`)
     }
     
-    if(target.id === message.author.id) return message.reply(`I can't ban you as you are the Boss`)
+    if(target.id === message.author.id) return message.reply(`No te podes autobanear, sos el ADMIN`)
     
     if(target.bannable) {
       let embed = new discord.MessageEmbed()
@@ -37,7 +37,7 @@ module.exports = {
       message.delete()
       
     } else {
-      return message.reply(`I can't ban them, make sure that my role is above of theirs`)
+      return message.reply(`No puedo hacer eso, su rol es mayor al mio`)
     }
     return undefined
   }
